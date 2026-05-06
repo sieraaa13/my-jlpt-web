392a823aa3fd580e2f32eff4ed3e78bf0a027122
siera <mh7714240@gmail.com>
2026-05-06 18:14:09 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/my-jlpt-web',
+  ...(process.env.VERCEL ? {} : { basePath: '/my-jlpt-web' }),
   images: {
     unoptimized: true,
   },
 };
-
 export default nextConfig;
