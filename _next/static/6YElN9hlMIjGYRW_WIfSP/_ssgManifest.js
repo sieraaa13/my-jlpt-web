15d98c15bedf28d7a51ae8212bfcaf0891ba15c2
sieraaa13 <sieraaa13@users.noreply.github.com>
2026-05-08 14:25:45 +0000

    deploy: 4179f057d553f5a62f6659cfd2640c00c30ff4bf

--- _next/static/6YElN9hlMIjGYRW_WIfSP/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[level]","\u002Fjlpt\u002F[category]","\u002Fjlpt\u002F[year]\u002F[period]","\u002Fjlpt\u002Fn3\u002Fsoumatome\u002F[week]\u002F[day]","\u002Fn3\u002Flesson\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
