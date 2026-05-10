3351194eacb7c0c3e4088bd2723a5407ac7ba624
siera <mh7714240@gmail.com>
2026-05-11 00:02:05 +0900

    Update getExamData.ts

--- lib/getExamData.ts
+++ lib/getExamData.ts
@@ -1,35 +1,62 @@
 import { exam201107 } from "../data/exams/2011/07";
+import { exam201112 } from "../data/exams/2011/12";
+import { exam201207 } from "../data/exams/2012/07";
+import { exam201212 } from "../data/exams/2012/12";
+import { exam201307 } from "../data/exams/2013/07";
+import { exam201312 } from "../data/exams/2013/12";
+import { exam201407 } from "../data/exams/2014/07";
+import { exam201412 } from "../data/exams/2014/12";
+import { exam201507 } from "../data/exams/2015/07";
+import { exam201512 } from "../data/exams/2015/12";
+import { exam201607 } from "../data/exams/2016/07";
+import { exam201612 } from "../data/exams/2016/12";
+import { exam201707 } from "../data/exams/2017/07";
+import { exam201712 } from "../data/exams/2017/12";
+import { exam201807 } from "../data/exams/2018/07";
+import { exam201812 } from "../data/exams/2018/12";
+import { exam201907 } from "../data/exams/2019/07";
+import { exam201912 } from "../data/exams/2019/12";
+import { exam202207 } from "../data/exams/2022/07";
+import { exam202212 } from "../data/exams/2022/12";
 
 // Data exam dipisah per level
-// Saat ini hanya N3 yang ada datanya
-// Untuk N1, N2, N4, N5 - akan return null sampai data tersedia
+// N3 punya banyak data, level lain belum tersedia
 const EXAMS_DATA_BY_LEVEL: Record<string, Record<string, any>> = {
   n3: {
     "2011-07": exam201107,
-    // Tambahkan data N3 lainnya di sini
-    // "2011-12": exam201112,
-    // "2012-07": exam201207,
+    "2011-12": exam201112,
+    "2012-07": exam201207,
+    "2012-12": exam201212,
+    "2013-07": exam201307,
+    "2013-12": exam201312,
+    "2014-07": exam201407,
+    "2014-12": exam201412,
+    "2015-07": exam201507,
+    "2015-12": exam201512,
+    "2016-07": exam201607,
+    "2016-12": exam201612,
+    "2017-07": exam201707,
+    "2017-12": exam201712,
+    "2018-07": exam201807,
+    "2018-12": exam201812,
+    "2019-07": exam201907,
+    "2019-12": exam201912,
+    "2022-07": exam202207,
+    "2022-12": exam202212,
+    // Tahun 2020, 2021, 2023, 2024, 2025 - belum ada data
   },
   
   // N1 - Belum ada data
-  n1: {
-    // "2011-07": exam201107_n1,
-  },
+  n1: {},
   
   // N2 - Belum ada data
-  n2: {
-    // "2011-07": exam201107_n2,
-  },
+  n2: {},
   
   // N4 - Belum ada data
-  n4: {
-    // "2011-07": exam201107_n4,
-  },
+  n4: {},
   
   // N5 - Belum ada data
-  n5: {
-    // "2011-07": exam201107_n5,
-  },
+  n5: {},
 };
 
 export async function getExamData(
