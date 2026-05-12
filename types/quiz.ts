bd9326e36605699392a00d7dc9557a2975d8edfa
siera <mh7714240@gmail.com>
2026-05-12 23:04:19 +0900

    Update quiz.ts

--- types/quiz.ts
+++ types/quiz.ts
@@ -1,1 +1,36 @@
+// /types/quiz.ts
 
+export interface QuizQuestion {
+  q: string;
+  opts: string[];
+  ans: number;
+  img_keyword: string;
+  img_cat: string;
+  explain: string;
+}
+
+export interface QuizTopic {
+  id: string;
+  name: string;
+  icon: string;
+  desc: string;
+}
+
+export interface QuizLevel {
+  name: string;
+  label: string;
+  diff: string;
+  ptCorrect: number;
+  ptStreak: number;
+  color: string;
+}
+
+export interface QuizDailyState {
+  date: string;
+  qUsed: number;
+  tUsed: number;
+  pts: number;
+  streak: number;
+  topicId: string;
+  lvl: number;
+}
