d55c0c50fca80d1bb3a9f0bcb19bf85f8815c175
sieraaa13 <mh7714240@gmail.com>
2026-09-01 18:04:44 +0900

    Add N2 Bunpou Week 5 Day 7, complete Week 5

--- data/n2/soumatome/lessons.ts
+++ data/n2/soumatome/lessons.ts
@@ -45,6 +45,7 @@ import w5d3 from "./week5_day3.json";
 import w5d4 from "./week5_day4.json";
 import w5d5 from "./week5_day5.json";
 import w5d6 from "./week5_day6.json";
+import w5d7 from "./week5_day7.json";
 
 export type GrammarSection = {
   pattern_title: string;
@@ -70,7 +71,7 @@ export const lessons: Record<string, Record<string, LessonFile>> = {
   "2": { "1": w2d1 as LessonFile, "2": w2d2 as LessonFile, "3": w2d3 as LessonFile, "4": w2d4 as LessonFile, "5": w2d5 as LessonFile, "6": w2d6 as LessonFile, "7": w2d7 as LessonFile },
   "3": { "1": w3d1 as LessonFile, "2": w3d2 as LessonFile, "3": w3d3 as LessonFile, "4": w3d4 as LessonFile, "5": w3d5 as LessonFile, "6": w3d6 as LessonFile, "7": w3d7 as LessonFile },
   "4": { "1": w4d1 as LessonFile, "2": w4d2 as LessonFile, "3": w4d3 as LessonFile, "4": w4d4 as LessonFile, "5": w4d5 as LessonFile, "6": w4d6 as LessonFile, "7": w4d7 as LessonFile },
-  "5": { "1": w5d1 as LessonFile, "2": w5d2 as LessonFile, "3": w5d3 as LessonFile, "4": w5d4 as LessonFile, "5": w5d5 as LessonFile, "6": w5d6 as LessonFile },
+  "5": { "1": w5d1 as LessonFile, "2": w5d2 as LessonFile, "3": w5d3 as LessonFile, "4": w5d4 as LessonFile, "5": w5d5 as LessonFile, "6": w5d6 as LessonFile, "7": w5d7 as LessonFile },
 };
 
 export function getOrganizedLessons() {
