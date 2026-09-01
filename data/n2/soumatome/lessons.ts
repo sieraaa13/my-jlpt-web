b66f71967641b89db5efc2ae7dffe6ef712a3d69
sieraaa13 <mh7714240@gmail.com>
2026-09-01 12:01:51 +0900

    Add N2 Bunpou Week 2 Day 1-2

--- data/n2/soumatome/lessons.ts
+++ data/n2/soumatome/lessons.ts
@@ -11,6 +11,10 @@ import w1d5 from "./week1_day5.json";
 import w1d6 from "./week1_day6.json";
 import w1d7 from "./week1_day7.json";
 
+// Week 2
+import w2d1 from "./week2_day1.json";
+import w2d2 from "./week2_day2.json";
+
 export type GrammarSection = {
   pattern_title: string;
   pattern_meaning?: string;
@@ -32,6 +36,7 @@ export type LessonFile = { levels: LessonLevel[]; };
 
 export const lessons: Record<string, Record<string, LessonFile>> = {
   "1": { "1": w1d1 as LessonFile, "2": w1d2 as LessonFile, "3": w1d3 as LessonFile, "4": w1d4 as LessonFile, "5": w1d5 as LessonFile, "6": w1d6 as LessonFile, "7": w1d7 as LessonFile },
+  "2": { "1": w2d1 as LessonFile, "2": w2d2 as LessonFile },
 };
 
 export function getOrganizedLessons() {
